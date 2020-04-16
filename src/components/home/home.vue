c767aa924433fb24fb680d0eb62e668d629ebdaf
xin1261492732 <1261492732@qq.com>
2020-04-16 23:17:32 +0800

    init

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,6 +1,12 @@
 <template>
   <el-container class="container">
-    <el-header class="header">Header</el-header>
+    <el-header class="header">
+      <el-row>
+        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="18"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="2"><div class="grid-content bg-purple"></div></el-col>
+      </el-row>
+    </el-header>
     <el-container>
       <el-aside class="aside" width="200px">Aside</el-aside>
       <el-main class="main">Main</el-main>
