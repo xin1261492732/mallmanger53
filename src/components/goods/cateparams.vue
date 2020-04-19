7a7bd1ff2150bb0a70004cb728b2973b08c77c7a
xin1261492732 <1261492732@qq.com>
2020-04-20 00:43:03 +0800

    完成数据

--- src/components/goods/cateparams.vue
+++ src/components/goods/cateparams.vue
@@ -16,9 +16,9 @@
     </el-form-item>
     </el-form>
 
-    <el-tabs v-model="active" @tab-click="handleClick">
+    <el-tabs v-model="active" @tab-click="handleClick" style="height: 300px; overflow: auto">
       <el-tab-pane label="动态参数" name="1">
-        <el-button type="danger" @click="canshu">设置动态参数</el-button>
+        <el-button type="danger" @click="canshu()">设置动态参数</el-button>
         <el-table :data="arrDyparams" style="width: 100%">
           <el-table-column type="expand" label="#">
             <template slot-scope="scope">
@@ -61,7 +61,7 @@
               <el-button size="mini"
                          plain type="danger"
                          icon="el-icon-delete"
-                         circle @click="showDeleUserMsgBox(scope.row.id)"></el-button>
+                         circle @click="showDeleUserMsgBox(scope.row.attr_id)"></el-button>
             </el-row>
             </template>
           </el-table-column>
@@ -69,6 +69,7 @@
         </el-table>
       </el-tab-pane>
       <el-tab-pane label="静态参数" name="2">
+        <el-button type="danger" @click="jingtai()">设置静态参数</el-button>
           <el-table :data="arrStaticparams" style="width: 100%">
             <el-table-column type="index" label="#">
             </el-table-column>
@@ -94,7 +95,7 @@
                 <el-button size="mini"
                            plain type="danger"
                            icon="el-icon-delete"
-                           circle @click="showDelecatMsgBox(scope.row.id)"></el-button>
+                           circle @click="showDelecatMsg(scope.row.attr_id)"></el-button>
               </el-row>
               </template>
             </el-table-column>
@@ -103,7 +104,7 @@
       </el-tab-pane>
     </el-tabs>
 
-    <!-- 编辑用户对话框-->
+    <!-- 动态编辑对话框-->
     <el-dialog title="编辑属性" :visible.sync="dialogFormVisibleEdit">
       <el-form :model="form">
         <el-form-item label="属性名称" label-width="100px">
@@ -116,24 +117,53 @@
       </div>
     </el-dialog>
 
-    <!-- 编辑用户对话框-->
-<!--    <el-dialog title="编辑属性" :visible.sync="dialogFormVisible">-->
-<!--      <el-form :model="form">-->
-<!--        <el-form-item label="属性名称" label-width="100px">-->
-<!--          <el-input  v-model="form.attr_name" autocomplete="off"></el-input>-->
-<!--        </el-form-item>-->
-<!--        <el-form-item label="动态数据" label-width="100px">-->
-<!--          <el-input  v-model="form.attr_sel" autocomplete="off"></el-input>-->
-<!--        </el-form-item>-->
-<!--        <el-form-item label="选项" label-width="100px">-->
-<!--          <el-input  v-model="form.attr_vals" autocomplete="off"></el-input>-->
-<!--        </el-form-item>-->
-<!--      </el-form>-->
-<!--      <div slot="footer" class="dialog-footer">-->
-<!--        <el-button @click="dialogFormVisible = false">取 消</el-button>-->
-<!--        <el-button type="primary" @click="editams()">确 定</el-button>-->
-<!--      </div>-->
-<!--    </el-dialog>-->
+    <!-- 动态添加对话框-->
+    <el-dialog title="编辑属性" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="属性名称" label-width="100px">
+          <el-input  v-model="form.attr_name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="动态数据" label-width="100px">
+          <el-input  v-model="form.attr_sel" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editams()">确 定</el-button>
+      </div>
+    </el-dialog>
+
+    <!-- 静态编辑对话框-->
+    <el-dialog title="编辑属性" :visible.sync="dialogFormVisibleaa">
+      <el-form :model="form2">
+        <el-form-item label="属性名称" label-width="100px">
+          <el-input  v-model="form2.attr_name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="属性值" label-width="100px">
+          <el-input  v-model="form2.attr_vals" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisibleaa = false">取 消</el-button>
+        <el-button type="primary" @click="editjin()">确 定</el-button>
+      </div>
+    </el-dialog>
+
+    <!-- 静态添加对话框-->
+    <el-dialog title="编辑属性" :visible.sync="dialogFormVisibletai">
+      <el-form :model="form">
+        <el-form-item label="属性名称" label-width="100px">
+          <el-input  v-model="form2.attr_name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="动态数据" label-width="100px">
+          <el-input  v-model="form2.attr_vals" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisibletai = false">取 消</el-button>
+        <el-button type="primary" @click="edittai()">确 定</el-button>
+      </div>
+    </el-dialog>
   </el-card>
 </template>
 
@@ -156,10 +186,16 @@ export default {
       arrStaticparams: [],
       dialogFormVisibleEdit: false,
       dialogFormVisible: false,
+      dialogFormVisibleaa: false,
+      dialogFormVisibletai: false,
       form: {
         attr_name: '',
-        attr_sel: 'many',
-        attr_vals: []
+        attr_sel: 'many'
+      },
+      form2: {
+        attr_name: '',
+        attr_sel: 'only',
+        attr_vals: ''
       }
     }
   },
@@ -167,11 +203,119 @@ export default {
     this.getGoodCate()
   },
   methods: {
+    // 静态添加  - 发送请求
+    async edittai () {
+      let putData = {
+        attr_name: this.form2.attr_name,
+        attr_vals: this.form2.attr_vals,
+        attr_sel: this.form2.attr_sel
+      }
+      const res = await this.$http.post(`categories/${this.selectedOptions[2]}/attributes`, putData)
+      console.log(res)
+      // 更新视图
+      this.handleClick()
+      this.dialogFormVisibletai = false
+    },
+    //  静态添加
+    jingtai () {
+      this.dialogFormVisibletai = true
+    },
+    // 删除静态 - 打开消息盒子
+    showDeleUserMsgBox (userId) {
+      this.$confirm('删除用户?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        const res = await this.$http.delete(`categories/${this.selectedOptions[2]}/attributes/${userId}`)
+        const {meta: {status, msg}} = res.data
+        if (status === 200) {
+          // 更新视图
+          this.handleChange()
+          // 提示
+          this.$message({
+            type: 'success',
+            message: msg
+          })
+        }
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
+    },
+    // 静态
+    async editjin () {
+      let putData = {
+        attr_name: this.form2.attr_name,
+        attr_sel: 'only',
+        attr_vals: this.form2.attr_vals
+      }
+      const res = await this.$http.put(`categories/${this.selectedOptions[2]}/attributes/${this.form2.attr_id}`, putData)
+      console.log(this.form2.attr_id)
+      console.log(res)
+      this.dialogFormVisibleaa = false
+      this.handleClick()
+    },
+    //  静态用户的编辑
+    showEditcatDia (user) {
+      this.form2 = user
+      this.dialogFormVisibleaa = true
+    },
+    // 删除用户 - 打开消息盒子
+    showDelecatMsg (userId) {
+      this.$confirm('删除用户?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        const res = await this.$http.delete(`categories/${this.selectedOptions[2]}/attributes/${userId}`)
+        const {meta: {status, msg}} = res.data
+        if (status === 200) {
+          // 更新视图
+          this.handleClick()
+          // 提示
+          this.$message({
+            type: 'success',
+            message: msg
+          })
+        }
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
+    },
+    // 添加
+    async editams () {
+      let putData = {
+        attr_name: this.form.attr_name,
+        attr_sel: this.form.attr_sel
+      }
+      const res = await this.$http.post(`categories/${this.selectedOptions[2]}/attributes`, putData)
+      // console.log(res)
+      // 更新视图
+      this.handleChange()
+      this.dialogFormVisible = false
+    },
+    // 添加  - 打开对话框
+    canshu () {
+      if (this.selectedOptions.length === 3) {
+        this.dialogFormVisible = true
+      }
+    },
     // 编辑 -发送请求
     async editUser () {
-      const res = await this.$http.put(`categories/${this.selectedOptions[2]}/attributes/${this.form.attr_id}`, this.form)
+      let putData = {
+        attr_name: this.form.attr_name,
+        attr_sel: 'many',
+        attr_vals: this.form.attr_vals.join(',')
+      }
+      const res = await this.$http.put(`categories/${this.selectedOptions[2]}/attributes/${this.form.attr_id}`, putData)
       console.log('-------')
-      console.log(this.form.attr_id)
+      console.log(this.form)
       this.dialogFormVisibleEdit = false
       // 更新视图
       this.handleChange()
@@ -180,6 +324,7 @@ export default {
     showEditUserDia (user) {
       // 获取用户数据
       this.form = user
+      console.log(this.form)
       this.dialogFormVisibleEdit = true
     },
     // 点击x按钮
@@ -226,7 +371,7 @@ export default {
           // 获取数据
           const res = await this.$http.get(`categories/${this.selectedOptions[2]}/attributes?sel=only`)
           this.arrStaticparams = res.data.data
-          console.log(this.arrDyparams)
+          console.log(this.arrStaticparams)
         }
       }
     },
@@ -242,7 +387,7 @@ export default {
             item.attr_vals.length === 0
               ? [] : item.attr_vals = item.attr_vals.trim().split(',')
         })
-        // console.log(this.arrDyparams)
+        console.log(this.arrDyparams)
       }
     },
     // 获取三级分类的信息
